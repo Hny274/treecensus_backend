8c0548df8b8bffcfe85b278ff2b2e716fb9eeffc
Hny274 <hunnygandhi274@gmail.com>
2023-11-06 18:53:50 +0530

    change in file

--- router/treecensus_route.js
+++ router/treecensus_route.js
@@ -5,12 +5,12 @@ const {treedata}=require("../treecensus/treecensus_data");
 //const {countTrees}=require("../treecensus/tree_count");
 const {countTreesByUser}=require("../treecensus/tree_count");
 const { map_pointer } = require("../treecensus/map_data");
-const { treedata_csv } = require("../treecensus/treedata_csv");
+const { exportToCsv } = require("../treecensus/treedata_csv");
 
 router.route("/treedata").post(treedata);
 //router.route("/countTrees").get(countTrees);
 router.route("/countTreesByUser").post(countTreesByUser);
 router.route("/map_pointer").get(map_pointer);
-router.route("/treedata_csv").get(treedata_csv);
+router.route("/exportToCsv").get(exportToCsv);
 
 module.exports=router;
--- treecensus/treedata_csv.js
+++ treecensus/treedata_csv.js
@@ -1,25 +1,53 @@
 const mysql = require("mysql");
 const conn = require("../db/connection");
-const fastcsv=require("fast-csv");
 
-const fs=require("fs");
-const ws=fs.createWriteStream("treedata.csv");
+const createCsvStringifier = require('csv-writer').createObjectCsvStringifier;
 
-const treedata_csv=async (req, res, next)=>{
-    conn.query("SELECT * FROM treedata",function(err,data){
-        if(err) throw err;
+const exportToCsv = async (req, res, next) => {
+    // SQL query to select all data from the treedata table
+    const sql = `
+        SELECT treeid, height, latitude, longitude, date, diameter, harmprac, health, owner, botanical, local, landmark, user
+        FROM treedata
+    `;
 
-        const jsonData=JSON.parse(JSON.stringify(data));
-        //console.log("jsonData",jsonData);
-        res.json({"status":"Success","message":"Write to treedata.csv successfully!","data":jsonData});
+    conn.query(sql, function (err, result) {
+        if (err) {
+            // Handling error and sending an error response
+            return res.status(500).json({ "Status": "Error", "message": err.message });
+        }
 
-        fastcsv.write(jsonData,{headers:true})
-        // .on("finish",function(){
-        //     console.log("Write to treedata.csv successfully!");
-        // })
-        .pipe(ws);
-        res.end();
-    })
-}
+        // Generating CSV data
+        const csvData = result;
 
-module.exports={treedata_csv};
+        // Setting up CSV stringifier with headers
+        const csvStringifier = createCsvStringifier({
+            header: [
+                { id: 'treeid', title: 'Tree ID' },
+                { id: 'height', title: 'Height' },
+                { id: 'latitude', title: 'Latitude' },
+                { id: 'longitude', title: 'Longitude' },
+                { id: 'date', title: 'Date' },
+                { id: 'diameter', title: 'Diameter' },
+                { id: 'harmprac', title: 'Harmprac' },
+                { id: 'health', title: 'Health' },
+                { id: 'owner', title: 'Owner' },
+                { id: 'botanical', title: 'Botanical' },
+                { id: 'local', title: 'Local' },
+                { id: 'landmark', title: 'Landmark' },
+                { id: 'user', title: 'User' }
+            ]
+        });
+
+        // Converting data to CSV format
+        const csvString = csvStringifier.stringifyRecords(csvData);
+
+        // Sending the CSV string as a response
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader('Content-Disposition', 'attachment; filename=tree_data.csv');
+        return res.status(200).send(csvString);
+    });
+};
+
+module.exports = { exportToCsv };
+
+
